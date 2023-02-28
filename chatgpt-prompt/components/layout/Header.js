620003cbdfa671d00c8405710e3130319948f1fe
Duke jojo <ysha7020@uni.sydney.edu.au>
2023-02-28 23:26:19 +1100

    homepage half done

---
+++ chatgpt-prompt/components/layout/Header.js
@@ -0,0 +1,56 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import { useState, useEffect } from "react";
+const Header = ({ handleOpen, headerStyle }) => {
+    const [scroll, setScroll] = useState(0)
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            const scrollCheck = window.scrollY > 100
+            if (scrollCheck !== scroll) {
+                setScroll(scrollCheck)
+            }
+        })
+    })
+    return (
+        <>
+            <header className={scroll ? `${headerStyle} header sticky-bar stick ` : `${headerStyle} header sticky-bar`}>
+                <div className="container">
+                    <div className="main-header">
+                        <div className="header-left">
+                            <div className="header-logo">
+                                <Link href="/" legacyBehavior>
+                                    <a className="d-flex">
+                                        {headerStyle ? <img alt="Agon" src="/assets/imgs/template/logo-white.svg" /> : <img alt="Agon" src="/assets/imgs/template/logo.svg" />}
+                                    </a>
+                                </Link>
+                            </div>
+                            <div className="header-nav">
+                                <nav className="nav-main-menu d-none d-xl-block">
+                                    <ul className="main-menu">
+                                        <li>
+                                            <Link href="#" legacyBehavior><a className="active"><i className="fi fi-rr-home" />主页</a></Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/blog-single" legacyBehavior><a><i className="fi fi-rr-document-signed" />GPT指南</a></Link>
+                                        </li>
+                                        <li>
+                                            <Link href="#" legacyBehavior><a><i className="fi fi-rr-headset" />关于我们</a></Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="header-right">
+                            <div className="block-signin">
+                                <Link href="/page-signup" legacyBehavior><a className="btn btn-default hover-up icon-arrow-right">Get Started</a></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+        </>
+    );
+};
+
+export default Header;
--- chatgpt-prompt/pages/_app.js
+++ chatgpt-prompt/pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import '../public/assets/css/style.css'
+import '../public/assets/css/modal.css'
+import "../public/assets/css/swiper-custom.css";
+import React, { useEffect, useState } from "react";
+import Preloader from '../components/elements/Preloader';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    if (typeof window !== "undefined") {
+      window.WOW = require("wowjs");
+    }
+    new WOW.WOW().init();
+  }, []);
+  return (
+    <>
+      {!loading ? (
+        <Component {...pageProps} />
+      ) : (
+        <Preloader />
+      )}
+
+    </>
+  )
 }
+
+export default MyApp
