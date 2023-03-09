1a3f7fb74288d9252bd784428c5e4bb5da3f3414
Duke jojo <ysha7020@uni.sydney.edu.au>
2023-03-09 13:23:59 +1100

    update

--- chatgpt-prompt/next.config.js
+++ chatgpt-prompt/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  "exportPathMap": async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+    }
+  }
 }
 
+
 module.exports = nextConfig
+
